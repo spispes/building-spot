c659dd9ae6657cd31e38d6bd3b80736d0bced811
Peter Spisak <peter@spisak.de>
2017-06-23 16:46:09 +0200

    carusell added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -9,18 +9,25 @@ import {routerConfig} from "router.config";
 import {RouterModule} from "@angular/router";
 import { CivilEngineeringComponent } from './civil-engineering/civil-engineering.component';
 import { BuildingConstructionComponent } from './building-construction/building-construction.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { JsonpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CivilEngineeringComponent,
-    BuildingConstructionComponent
+    BuildingConstructionComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
+    JsonpModule,
+    NgbModule,
     RouterModule.forRoot(routerConfig)
   ],
   providers: [],
