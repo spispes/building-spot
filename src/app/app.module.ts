9dd837b96da05693e97d8cc0d69e5a72309b4364
Peter Spisak <peter@spisak.de>
2017-06-29 16:54:21 +0200

    google map added new component map included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {routerConfig} from "router.config";
 import {RouterModule} from "@angular/router";
-import { CivilEngineeringComponent } from './civil-engineering/civil-engineering.component';
-import { BuildingConstructionComponent } from './building-construction/building-construction.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { JsonpModule } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {CivilEngineeringComponent} from './civil-engineering/civil-engineering.component';
+import {BuildingConstructionComponent} from './building-construction/building-construction.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {JsonpModule} from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NguiMapModule} from '@ngui/map';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HomeComponent,
     CivilEngineeringComponent,
     BuildingConstructionComponent,
-    CarouselComponent
+    CarouselComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
@@ -28,9 +31,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     JsonpModule,
     NgbModule,
+    NguiMapModule.forRoot({
+      apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyBY-v3vIjnVSbqWxoJS08jFmhAFRnwF2Ls' +
+      '&libraries=visualization,places,drawing'
+    }),
     RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
